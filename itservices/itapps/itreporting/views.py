8dbd590098137a9db43f30472241e68198adb864
emperorthimy <olagokethimy@gmail.com>
2023-03-02 10:12:36 +0000

    Django Apps

--- itservices/itapps/itreporting/views.py
+++ itservices/itapps/itreporting/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 def home(request):
-    return HttpResponse('<h1> Student IT Services - Home </h1>')
+    return render(request, 'itreporting/home.html')
 
 def about(request):
-    return HttpResponse('<h1> Student IT Services - About Us </h1>')
+    return render(request, 'itreporting/about.html')
 
 def contact(request):
-    return HttpResponse('<h1> Student IT Services - Contact Us </h1>')
+    return render(request, 'itreporting/contact.html')
